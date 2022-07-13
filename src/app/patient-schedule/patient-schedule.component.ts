4070193d2b4014ae08a7f7f4dfc3f311f404aa18
Amanda-RVargas <amanda.jaques.vargas@outlook.com>
2022-07-13 01:08:24 -0300

    consuming api patient and delete patient

--- src/app/patient-schedule/patient-schedule.component.ts
+++ src/app/patient-schedule/patient-schedule.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Patient } from '../receptionist-panel/patient';
+import { ServiceAuthService } from '../services/patients.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-patient-schedule',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientScheduleComponent implements OnInit {
 
-  constructor() { }
+    patient = {} as Patient;
+
+  constructor( private patientService: ServiceAuthService,
+    private route: Router) {  }
 
   ngOnInit(): void {
+    this.showPatient();
+    
+  }
+
+  showPatient(): void {
+    this.patientService.getPatients(
+    sessionStorage.getItem("token"), 
+    sessionStorage.getItem("companyId"),
+    sessionStorage.getItem("patientId"))
+    .subscribe(
+      data => {
+        this.patient = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  deletePatient(name: any, id: any) {
+    if(confirm("Tem certeza que deseja deletar a paciente " + name + "?")) {
+      this.patientService.deletePatient(sessionStorage.getItem("token"), 
+      sessionStorage.getItem("companyId"),id)
+      .subscribe(
+        data => {
+          this.route.navigateByUrl('/receptionist-panel');
+        },
+        error => {
+          console.log(error);
+        }
+      );
+      this.route.navigateByUrl('/receptionist-panel');
+
+    }
   }
 
 }
